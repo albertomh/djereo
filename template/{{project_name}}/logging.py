741e58ae9b26dacde0a2556672dc50ac58cfa8e1
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-01-30 19:01:44 +0000

    test: config returned by LoggingConfigFactory derived from DEBUG

--- template/{{project_name}}/logging.py
+++ template/{{project_name}}/logging.py
@@ -1,15 +1,6 @@
-import logging
+class LoggingConfigFactory:
+    def __init__(self, debug=False):
+        self.debug = debug
 
-import structlog
-
-
-def configure_logging():
-    structlog.configure(
-        processors=[
-            structlog.stdlib.add_log_level,
-            structlog.processors.TimeStamper(fmt="iso"),
-            structlog.processors.JSONRenderer(),
-        ],
-        wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
-        cache_logger_on_first_use=True,
-    )
+    def build(self):
+        return {}
