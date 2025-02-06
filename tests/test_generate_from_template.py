996b80947606b7f9cea505a40199cb34ec1859b0
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-06 22:18:28 +0000

    fix: revert adding a .env.ci file

--- tests/test_generate_from_template.py
+++ tests/test_generate_from_template.py
@@ -49,7 +49,7 @@ def test_djereo_jinja_templates_converted(
 @pytest.mark.integration
 @pytest.mark.parametrize(
     "is_github_project, expected_directory_count, expected_file_count",
-    [(True, 6, 20), (False, 5, 18)],
+    [(True, 6, 19), (False, 5, 17)],
 )
 def test_is_github_project(
     is_github_project: bool,
