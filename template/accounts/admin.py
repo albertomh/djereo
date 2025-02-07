8055fa51a5e5bb76da1ad450bb22a27a1bf5790b
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-07 08:37:54 +0000

    feat: custom 'AuthUser' model to override django.contrib.auth's

--- template/accounts/admin.py
+++ template/accounts/admin.py
@@ -1,1 +1,15 @@
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from accounts.forms import AuthUserChangeForm, AuthUserCreationForm
+from accounts.models import AuthUser
+
+
+class AuthUserAdmin(UserAdmin):
+    add_form = AuthUserCreationForm
+    form = AuthUserChangeForm
+    model = AuthUser
+    list_display = ["email", "username"]
+
+
+admin.site.register(AuthUser, AuthUserAdmin)
