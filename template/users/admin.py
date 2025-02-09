34b447ee230422ac1e9f678fd0a27bb3d5ad70d2
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-09 09:18:53 +0000

    feat: add UserProfile model to gen. projects

--- template/users/admin.py
+++ template/users/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
 from users.forms import AuthUserChangeForm, AuthUserCreationForm
-from users.models import AuthUser
+from users.models import AuthUser, UserProfile
+
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    show_change_link = True
+    verbose_name_plural = "User Profile"
 
 
 @admin.register(AuthUser)
@@ -11,3 +18,7 @@ class AuthUserAdmin(UserAdmin):
     form = AuthUserChangeForm
     model = AuthUser
     list_display = ["email", "username"]
+    inlines = [UserProfileInline]
+
+
+admin.site.register(UserProfile)
