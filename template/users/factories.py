2a6ade325342bbba7bed0b6114bddf1296f755a1
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-12 08:56:26 +0000

    refactor: disable AuthUser post_save signal in AuthUserFactory

--- template/users/factories.py
+++ template/users/factories.py
@@ -2,6 +2,7 @@ from factory import Faker, SubFactory
 from factory.django import DjangoModelFactory
 
 from users.models import AuthUser, UserProfile
+from users.signals import disable_authuser_signal
 
 
 class AuthUserFactory(DjangoModelFactory):
@@ -13,6 +14,16 @@ class AuthUserFactory(DjangoModelFactory):
     first_name = Faker("first_name")
     last_name = Faker("last_name")
     password = Faker("password")
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        """Override FactoryBoy's default create() to disable signals.
+
+        Signals disabled because creating an AuthUser would create a UserProfile, which we
+        wish to avoid and instead do explicitly via a subfactory in UserProfileFactory.
+        """
+        with disable_authuser_signal():
+            return super()._create(model_class, *args, **kwargs)
 
 
 class UserProfileFactory(DjangoModelFactory):
