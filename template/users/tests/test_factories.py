102ef31828dc4af3e0ac01099f20df5e43b529d3
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-12 08:52:13 +0000

    feat(test): unit tests for AuthUser and UserProfile factories

---
+++ template/users/tests/test_factories.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+
+from users.factories import AuthUserFactory, UserProfileFactory
+from users.models import AuthUser, UserProfile
+
+
+class AuthUserFactoryTest(TestCase):
+    def test_authuser_factory_creates_user(self):
+        user = AuthUserFactory()
+
+        self.assertIsInstance(user, AuthUser)
+        self.assertTrue(AuthUser.objects.filter(id=user.id).exists())
+
+
+class UserProfileFactoryTest(TestCase):
+    def test_userprofile_factory_creates_profile(self):
+        profile = UserProfileFactory()
+
+        self.assertIsInstance(profile, UserProfile)
+        self.assertTrue(UserProfile.objects.filter(id=profile.id).exists())
