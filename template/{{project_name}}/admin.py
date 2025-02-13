0ffba6494d1b06666e856f6f7f4d086ef0f0b7b0
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2025-02-13 07:29:12 +0000

    feat: Django admin login form reads 'Email' instead of 'Username' (#98)
    * feat(test): custom admin login form says 'Email' instead of 'Username'
    
    * feat: custom admin login form says 'Email' instead of 'Username'

---
+++ template/{{project_name}}/admin.py
@@ -0,0 +1,15 @@
+from django.contrib.admin import AdminSite
+from django.contrib.auth.forms import AuthenticationForm
+
+
+class CustomAdminAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["username"].label = "Email"
+
+
+class CustomAdminSite(AdminSite):
+    login_form = CustomAdminAuthenticationForm
+
+
+admin_site = CustomAdminSite()
