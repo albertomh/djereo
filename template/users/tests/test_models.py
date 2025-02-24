627ff8a6c1908b3a682b10dda57e1472010a6937
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-02-24 17:53:51 +0000

    feat(test): check user model has an updated_at timestamp

--- template/users/tests/test_models.py
+++ template/users/tests/test_models.py
@@ -1,7 +1,21 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-from users.models import UserProfile
+from users.models import AuthUser, UserProfile
+
+
+class AuthUserTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="spanza",
+            email="escudero@quijano.es",
+            password="equusasinus",
+        )
+
+    def test_timestamps_exist(self):
+        profile = AuthUser.objects.get(username=self.user.username)
+        self.assertIsNotNone(profile.updated_at)
 
 
 class UserProfileTestCase(TestCase):
