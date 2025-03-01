12e5aa6674ab3fac9694080a2eacbb6709f50586
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2025-03-01 09:50:24 +0000

    refactor(test): optionally get URLs as part of wait_for_server_start (#139)
    * refactor(test): dry up test_integration calls to runserver
    
    * refactor(test): optionally get URLs as part of wait_for_server_start

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -14,6 +14,30 @@ from sh import TimeoutException, just
 from tests._utils import remove_ansi_escape_codes
 
 
+def wait_for_server_start(
+    out: StringIO,
+    *,
+    timeout=10.0,
+    interval=0.1,
+    urls_to_get: list[tuple[str, int]] = None,
+) -> bool:
+    if urls_to_get is None:
+        urls_to_get = []
+    sentinel = "Quit the server with CONTROL-C."
+    start = time.time()
+
+    while time.time() - start < timeout:
+        if sentinel in out.getvalue():
+            for url, expected_status in urls_to_get:
+                with urlopen(url) as response:
+                    time.sleep(interval)
+                    assert response.status == expected_status
+            return True
+        time.sleep(interval)
+
+    return False
+
+
 @pytest.mark.integration
 @pytest.mark.slow
 def test_sys_check_warn_no_dev_mode_when_debug(
@@ -31,34 +55,27 @@ def test_sys_check_warn_no_dev_mode_when_debug(
     copier_copy(copier_input_data)
     set_up_test_database()
     out, err = StringIO(), StringIO()
-    timeout, interval = 10, 0.1  # seconds
     expected_warning = (
         f"({test_project_name}.W001) Python Development Mode is not enabled yet DEBUG is"
         " true."
     )
 
-    sentinel = "Quit the server with CONTROL-C."
-    start = time.time()
     with suppress(TimeoutException):
         just(
-            # PYTHONDEVMODE can only be disabled by setting it to an empty string
             "runserver",
+            # PYTHONDEVMODE can only be disabled by setting it to an empty string
             "",
-            _timeout=timeout,
+            _timeout=10,
             _bg=True,
             _bg_exc=False,
             _out=out,
             _err=err,
             _cwd=test_project_dir,
         )
-        while (time.time() - start) < timeout:
-            if sentinel in out.getvalue():
-                clean_stderr = remove_ansi_escape_codes(err.getvalue())
-                assert expected_warning in clean_stderr
-                return
-            time.sleep(interval)
+        assert wait_for_server_start(out), "Django runserver did not start in time"
 
-    raise AssertionError("Django runserver did not start in time")
+    clean_stderr = remove_ansi_escape_codes(err.getvalue())
+    assert expected_warning in clean_stderr
 
 
 @pytest.mark.integration
@@ -73,27 +90,20 @@ def test_runserver(
     copier_copy(copier_input_data)
     set_up_test_database()
     out = StringIO()
-    timeout, interval = 10, 0.1  # seconds
 
-    sentinel = "Quit the server with CONTROL-C."
-    start = time.time()
     with suppress(TimeoutException):
         just(
             "runserver",
-            _timeout=timeout,
+            _timeout=10,
             _bg=True,
             _bg_exc=False,
             _out=out,
             _cwd=test_project_dir,
         )
-        while (time.time() - start) < timeout:
-            if sentinel in out.getvalue():
-                start_message = "Starting development server at http://127.0.0.1:8000/"
-                assert start_message in out.getvalue()
-                return
-            time.sleep(interval)
+        assert wait_for_server_start(out), "Django runserver did not start in time"
 
-    raise AssertionError("Django runserver did not start in time")
+    start_message = "Starting development server at http://127.0.0.1:8000/"
+    assert start_message in out.getvalue()
 
 
 @pytest.mark.integration
@@ -108,30 +118,24 @@ def test_django_debug_toolbar_is_enabled(
     copier_copy(copier_input_data)
     set_up_test_database()
     out = StringIO()
-    timeout, interval = 10, 0.1  # seconds
 
-    sentinel = "Quit the server with CONTROL-C."
-    start = time.time()
     with suppress(TimeoutException):
         just(
             "runserver",
-            _timeout=timeout,
+            _timeout=10,
             _bg=True,
             _bg_exc=False,
             _out=out,
             _cwd=test_project_dir,
         )
-        while (time.time() - start) < timeout:
-            if sentinel in out.getvalue():
-                with urlopen("http://127.0.0.1:8000/") as response:
-                    res_bytes = response.read()
-            time.sleep(interval)
+        assert wait_for_server_start(out), "Django runserver did not start in time"
+    with urlopen("http://127.0.0.1:8000/") as response:
+        res_bytes = response.read()
+    res_html = res_bytes.decode("utf8")
+    html = BeautifulSoup(res_html)
+    dj_debug_toolbar = html.find("div", {"id": "djDebug"})
 
-        res_html = res_bytes.decode("utf8")
-        html = BeautifulSoup(res_html)
-        dj_debug_toolbar = html.find("div", {"id": "djDebug"})
-
-        assert type(dj_debug_toolbar) is Tag
+    assert type(dj_debug_toolbar) is Tag
 
 
 @pytest.mark.integration
@@ -146,24 +150,20 @@ def test_runserver_dev_logs_use_rich(
     copier_copy(copier_input_data)
     set_up_test_database()
     out = StringIO()
-    timeout, interval = 10, 0.1  # seconds
 
-    sentinel = "Quit the server with CONTROL-C."
-    start = time.time()
     with suppress(TimeoutException):
         just(
             "runserver",
-            _timeout=timeout,
+            _timeout=10,
             _bg=True,
             _bg_exc=False,
             _out=out,
             _cwd=test_project_dir,
         )
-        while (time.time() - start) < timeout:
-            if sentinel in out.getvalue():
-                with urlopen("http://127.0.0.1:8000/") as response:
-                    assert response.status == 200
-            time.sleep(interval)
+        urls_to_get = [("http://127.0.0.1:8000/", 200)]
+        assert wait_for_server_start(out, urls_to_get=urls_to_get), (
+            "Django runserver did not start in time"
+        )
 
     pattern = r"\[\d{2}:\d{2}:\d{2}\] INFO\s+ 200 GET \/ HTTP\/1\.1\s+"
     clean_stdout = remove_ansi_escape_codes(out.getvalue())
@@ -182,32 +182,24 @@ def test_django_allauth_pages_exist(
     tear_down_test_database,
 ):
     allauth_urls = [
-        "/accounts/login/",
-        "/accounts/signup/",
+        "accounts/login/",
+        "accounts/signup/",
     ]
     copier_copy(copier_input_data)
     set_up_test_database()
     just("manage", "migrate", _cwd=test_project_dir)
     out = StringIO()
-    timeout, interval = 15, 0.1  # seconds
 
-    sentinel = "Quit the server with CONTROL-C."
-    start = time.time()
     with suppress(TimeoutException):
         just(
             "runserver",
-            _timeout=timeout,
+            _timeout=10,
             _bg=True,
             _bg_exc=False,
             _out=out,
             _cwd=test_project_dir,
         )
-        while (time.time() - start) < timeout:
-            if sentinel in out.getvalue():
-                for url in allauth_urls:
-                    with urlopen(f"http://127.0.0.1:8000{url}") as response:
-                        assert response.status == 200
-                return
-            time.sleep(interval)
-
-    raise AssertionError("Django runserver did not start in time")
+        urls_to_get = [(f"http://127.0.0.1:8000/{url}", 200) for url in allauth_urls]
+        assert wait_for_server_start(out, urls_to_get=urls_to_get), (
+            "Django runserver did not start in time"
+        )
