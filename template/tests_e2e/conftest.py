d163a5ced1847998f44b9237335cb6ffd8237e61
Alberto Morón Hernández <alberto.moronh@gmail.com>
2025-08-23 10:37:50 +0100

    feat(e2e): check webapp is available before attempting e2e run

--- template/tests_e2e/conftest.py
+++ template/tests_e2e/conftest.py
@@ -2,9 +2,12 @@
 # Locally, set the CI environment variable to run in headless browser:
 # >>> CI=true uv run pytest tests_e2e/
 
+import sys
+
 import pytest
 from environs import Env
 from playwright.sync_api import Browser, Playwright, sync_playwright
+from playwright.sync_api import Error as PlaywrightError
 
 env = Env()
 env.read_env()
@@ -18,6 +21,24 @@ def _launch_browser(headless: bool = True) -> tuple[Playwright, Browser]:
     return playwright, browser
 
 
+def pytest_sessionstart(session):
+    """Check webapp is available before attempting to collect and run the test suite."""
+    try:
+        playwright, browser = _launch_browser(True)
+        context = browser.new_context()
+        page = context.new_page()
+        page.goto(BASE_URL, timeout=3000)
+
+    except PlaywrightError as e:
+        print(e, file=sys.stderr)  # noqa: T201
+        pytest.exit("Startup check failed", returncode=1)
+
+    finally:
+        context.close()
+        browser.close()
+        playwright.stop()
+
+
 @pytest.fixture(scope="session")
 def browser():
     """Start Playwright Chromium once per test session."""
@@ -26,7 +47,6 @@ def browser():
     headless = is_ci or is_gha
     playwright, browser = _launch_browser(headless)
     yield browser
-
     browser.close()
     playwright.stop()
 
@@ -36,7 +56,5 @@ def page(browser):
     """Provide a fresh page for each test."""
     context = browser.new_context()
     page = context.new_page()
-
     yield page
-
     context.close()
