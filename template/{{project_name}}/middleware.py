27b7b25faecaacc7749e07d97b6d87b975ea94e9
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2026-03-03 08:13:37 +0000

    docs: comment Cross-Origin headers in middleware (#344)

--- template/{{project_name}}/middleware.py
+++ template/{{project_name}}/middleware.py
@@ -7,7 +7,14 @@ class SecurityHeadersMiddleware:
     def __call__(self, request):  # pragma: no cover
         response = self.get_response(request)
 
+        # 'Cross-Origin-Opener-Policy' has default "same-origin"
+        # this isolates browser window from cross-origin documents
+        # <https://docs.djangoproject.com/en/6.0/ref/settings/#secure-cross-origin-opener-policy>
+
+        # prevents your document from loading any cross-origin resources
+        # (unless explicitly permitted via a CORP header)
         response["Cross-Origin-Embedder-Policy"] = "require-corp"
+        # prevents the document from being loaded by cross-origin resources
         response["Cross-Origin-Resource-Policy"] = "same-origin"
 
         return response
